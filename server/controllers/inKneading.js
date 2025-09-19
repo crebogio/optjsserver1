fa62d9676284f6be4fe356133a2e99b27bf134af
crebogio <50924185+crebogio@users.noreply.github.com>
2025-09-19 00:14:04 -0700

    Update inKneading.js

--- server/controllers/inKneading.js
+++ server/controllers/inKneading.js
@@ -2,7 +2,7 @@ const { checkSeirenPlan, checkSeirenWIP, checkUser, checkMachine, entryWIP, entr
 const CustomError = require("../error/custom-error");
 
 const inKneading = async (req, res) => {
-  const { process, user, machine, transNum, transNumBatch,batchNo,mixture  } = req.body;
+  const { user, machine, transNum, batchNo,mixture  } = req.body;
   
   const isTransNumInPlan = await checkSeirenPlan(transNum);
   const isTransNumInWIP = await checkSeirenWIP(transNum);
@@ -15,8 +15,8 @@ const inKneading = async (req, res) => {
         if (isTransNumInWIP.length === 0){
           if(isTransNumInPlan.length > 0){
             if(isMixtureValid.length > 0){
-              const itemEntry = await entryWIP(process,user, machine, transNum,"N/A",batchNo);
-              const logEntry = await entryLogs("WIP", process, user, machine, transNum,"N/A", "N/A", "0.0",batchNo);
+              const itemEntry = await entryWIP("KNEADING",user, machine, transNum,"N/A",batchNo);
+              const logEntry = await entryLogs("WIP", "KNEADING", user, machine, transNum,"N/A", "N/A", "0.0",batchNo);
               res.status(200).json({message:'Valid'}); 
             }
             else{
