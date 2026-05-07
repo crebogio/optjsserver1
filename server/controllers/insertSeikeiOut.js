4a8bacd6fb41320ca6d94e96a60f431608ae7e39
crebogio <50924185+crebogio@users.noreply.github.com>
2026-05-07 08:05:00 -0700

    added lot_no

--- server/controllers/insertSeikeiOut.js
+++ server/controllers/insertSeikeiOut.js
@@ -3,10 +3,10 @@ const { dbDeleteSeikeiWip, dbInsertSeikeiOut} = require("../db/database");
 const CustomError = require("../error/custom-error");
 
 const insertSeikeiOut = async (req, res) => {
-  const { ctrl_no,item_no, mold_no, batch_no,machine,employee,start_time,end_time,no_of_shots,produce_count,reject_count,reject_type} = req.body; 
+  const { ctrl_no,item_no, mold_no, batch_no,lot_no,machine,employee,start_time,end_time,no_of_shots,produce_count,reject_count,reject_type} = req.body; 
 
   const xx = await dbDeleteSeikeiWip(ctrl_no);
-  const yy = await dbInsertSeikeiOut(ctrl_no,item_no, mold_no, batch_no,machine,employee,start_time,end_time,no_of_shots,produce_count,reject_count,reject_type);
+  const yy = await dbInsertSeikeiOut(ctrl_no,item_no, mold_no, batch_no,lot_no,machine,employee,start_time,end_time,no_of_shots,produce_count,reject_count,reject_type);
   res.status(200).json({message:'Insert Out Ok'}); 
   
 };
--- server/controllers/insertSeikeiWip.js
+++ server/controllers/insertSeikeiWip.js
@@ -3,14 +3,14 @@ const { dbCheckSeikeiEmployee, dbCheckSeikeiMachine,dbInsertSeikeiWip} = require
 const CustomError = require("../error/custom-error");
 
 const insertSeikeiWip = async (req, res) => {
-  const { ctrl_no,item_no,mold_no,batch_no,machine,employee,start_time} = req.body; 
+  const { ctrl_no,item_no,mold_no,batch_no,lot_no,machine,employee,start_time} = req.body; 
 
   const isEmployeeValid = await dbCheckSeikeiEmployee(employee);
   const isMachineValid = await dbCheckSeikeiMachine(machine);
 
   if (isEmployeeValid.length > 0) {
     if (isMachineValid.length > 0) {
-          const itemEntry = await dbInsertSeikeiWip(ctrl_no,item_no, mold_no, batch_no,machine,employee,start_time);
+          const itemEntry = await dbInsertSeikeiWip(ctrl_no,item_no, mold_no, batch_no,lot_no,machine,employee,start_time);
           res.status(200).json({message:'Insert WIP Ok'}); 
     }
     else{
